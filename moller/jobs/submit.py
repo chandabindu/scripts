c9bd4fa1a81914bf0943cd6914546401fab98b71
Chandan Ghosh <chandan@ifarm1901.jlab.org>
2021-05-28 12:35:21 -0400

    New job submission scripts

--- moller/jobs/submit.py
+++ moller/jobs/submit.py
@@ -3,59 +3,36 @@ import os
 import subprocess
 import time
 
-if not sys.argv[1]:
-        sourceDir = "/lustre19/expphy/volatile/halla/moller12gev/chandan/sim_out"
-        #sourceDir = "/lustre19/expphy/volatile/halla/parity/chandan/sim_out"
-else:
-        sourceDir = sys.argv[1]
-if not sys.argv[2]:
-        config = "AsymmetricCoil4_ep"
-else:
-        config = sys.argv[2]
+sourceDir = "/w/halla-scifs17exp/parity/disk1/chandan/gitdir/remoll"
+config = "test"
+generator = "beam"
 
-if not sys.argv[3]:
-        generator = "ep"
-else:
-        generator = sys.argv[3]
+runrange= range(int(sys.argv[1]),int(sys.argv[2]))
 
-runrange= range(int(sys.argv[4]),int(sys.argv[5]))
+outDir="/lustre19/expphy/volatile/halla/moller12gev/chandan/sim_out/"+config
+jobs=outDir+"/jobs"
 
-jobs=sourceDir+"/"+config+"/jobs"
-outDir=sourceDir+"/"+config+"/output"
-if not os.path.exists(jobs):
-  os.system("mkdir "+jobs)
 if not os.path.exists(outDir):
   os.system("mkdir "+outDir)
+if not os.path.exists(jobs):
+  os.system("mkdir "+jobs)
 
-
-home = sourceDir+"/"+config
 
 for i in runrange:
-  filename=sourceDir+"/"+config+"/remollout_"+generator+str(i)+".root"
-  outfile=outDir+"/analysis_"+str(i)+".root"
-  if os.path.exists(filename):
-    jsubf=open(jobs+"/ep_"+str(i)+".sh", "w")
-    jsubf.write("#!/bin/bash\n")
-    jsubf.write("#SBATCH --account=halla\n")
-    jsubf.write("#SBATCH --partition=production\n")
-    jsubf.write("#SBATCH --job-name=remollAna\n")
-    jsubf.write("#SBATCH --time=00:05:00\n")
-    jsubf.write("#SBATCH --nodes=1\n")
-    jsubf.write("#SBATCH --ntasks=1\n")
-    jsubf.write("#SBATCH --cpus-per-task=1\n")
-    jsubf.write("#SBATCH --mem=3G\n")
-    jsubf.write("tcsh -c \"source /apps/root/6.18.00/setroot_CUE\"\n")
-    jsubf.write("#SBATCH --chdir="+outDir+"\n")
-    #jsubf.write("#SBATCH --output="+outDir+"/tmp"+"_"+str(i)+".out\n")
-    #jsubf.write("#SBATCH --output=/lustre19/expphy/volatile/halla/moller12gev/chandan/sim_out/OffsetCoil3_ep/jobs/out.out\n")
-    jsubf.write("#SBATCH --output=/lustre19/expphy/volatile/halla/parity/chandan/sim_out/OffsetCoil3_ep/jobs/out.out\n")
-    #jsubf.write("#SBATCH --error="+outDir+"/tmp"+"_"+str(i)+".err\n")
-    jsubf.write("cd "+home+"\n")
-    jsubf.write("echo \"Current working directory is `pwd`\"\n")	
-    jsubf.write("./reroot -q -b ShiftedCoil_BoreVsAcceptance.C'(\""+filename+"\", \""+outfile+"\")'\n")
-    print("sbatch "+jobs+"/ep_"+str(i)+".sh") 
-      		
-    
-		
-	
-	
+  outfile=outDir+"/test_"+generator+"_"+str(i)+".root"
+  jsubf=open(jobs+"/test_"+str(i)+".sh", "w")
+  jsubf.write("#!/bin/bash\n")
+  jsubf.write("#SBATCH --account=halla\n")
+  jsubf.write("#SBATCH --partition=production\n")
+  jsubf.write("#SBATCH --job-name=TEST\n")
+  jsubf.write("#SBATCH --time=00:05:00\n")
+  jsubf.write("#SBATCH --nodes=1\n")
+  jsubf.write("#SBATCH --ntasks=1\n")
+  jsubf.write("#SBATCH --cpus-per-task=1\n")
+  jsubf.write("#SBATCH --mem=2G\n")
+  jsubf.write("tcsh -c \"source /apps/root/6.18.00/setroot_CUE\"\n")
+  jsubf.write("cd "+sourceDir+"\n")
+  jsubf.write("echo \"Current working directory is `pwd`\"\n")	
+  jsubf.write("./build/remoll -o "+outfile+" macros/runexample.mac\n")
+  print("sbatch "+jobs+"/test_"+str(i)+".sh") 
+    		
--- moller/jobs/submit.sh
+++ moller/jobs/submit.sh
@@ -2,87 +2,10 @@
 
 
 rm command100
-rm command200
-rm command300
-rm command400
-rm command500
-rm command600
-rm command900
-rm command1000
-rm command1100
-
-
 
 touch command100
-touch command200
-touch command300
-touch command400
-touch command500
-touch command600
-touch command700
-touch command800
-touch command900
-touch command1000
-touch command1100
-
 
 # rm *.out
-python submit.py "" "" "" 1 101 >> command100
+python submit.py 1 11 >> command100
 sh command100
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 101 201 >> command200
-sh command200
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 201 301 >> command300
-sh command300
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 301 401 >> command400
-sh command400
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 401 501 >> command500
-sh command500
-sleep 300
-
-
-# rm *.out
-python submit.py "" "" "" 501 601 >> command600
-sh command600
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 601 701 >> command700
-sh command700
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 701 801 >> command800
-sh command800
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 801 901 >> command900
-sh command900
-sleep 300
-
-# rm *.out
-python submit.py "" "" "" 901 1001 >> command1000
-sh command1000
-sleep 300
-
-rm *.out
-python submit.py "" "" "" 1001 1101 >> command1100
-sh command1100
-sleep 300
-
-
-
-
-
+sleep 10
